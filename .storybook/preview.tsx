4ca4dd82571098665156d054fd339aad94a640c3
Jason Belmonti <jasonbelmonti@gmail.com>
2026-03-06 15:51:22 -0600

    Fix shared theme asset loading

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,9 @@
 import type { Preview } from "@storybook/react-webpack5";
 
 import "antd/dist/reset.css";
-import "@fontsource/azeret-mono/400.css";
-import "@fontsource/azeret-mono/500.css";
-import "@fontsource/oxanium/500.css";
-import "@fontsource/oxanium/600.css";
-import "@fontsource/oxanium/700.css";
-import "../src/styles/theme.css";
 import "../src/styles/storybook.css";
 
-import { AntdThemeProvider } from "../src/providers/AntdThemeProvider";
+import { AntdThemeProvider } from "../src";
 
 const preview: Preview = {
   decorators: [
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import "@fontsource/azeret-mono/400.css";
+import "@fontsource/azeret-mono/500.css";
+import "@fontsource/oxanium/500.css";
+import "@fontsource/oxanium/600.css";
+import "@fontsource/oxanium/700.css";
+import "./styles/theme.css";
+
 export { AntdThemeProvider, installStaticAntdTheme } from "./providers/AntdThemeProvider";
 export { marathonDosTheme } from "./theme/marathonDosTheme";
